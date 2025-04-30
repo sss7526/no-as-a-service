fccb347b6cf0796d79eb666de192d6911a04bc5a
Daniil Mira <github@daniilmira.com>
2025-04-30 19:01:13 +0200

    Add: Basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let ip: String = env::var("NOAAS_IP").unwrap_or("0.0.0.0".to_string());
+    let port: String = env::var("NOAAS_PORT").unwrap_or("3000".to_string());
+    let address: String = format!("{}:{}", ip, port);
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind(address.as_str()).await.unwrap();
+    println!("No As a Service is running at: {}", address);
+    axum::serve(listener, app).await.unwrap();
 }
